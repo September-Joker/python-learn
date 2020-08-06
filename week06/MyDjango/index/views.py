1b0782af7e9403c419fb2830e1c248c0bf62f12d
September-Joker <ma857714903@gmail.com>
2020-08-06 22:59:28 +0800

    settings.py修改为使用mysql数据库，创建test数据库下数据，修改urls.py文件和views视图文件

--- week06/MyDjango/index/views.py
+++ week06/MyDjango/index/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello Django!')
+
+
+def year(request, year):
+    # return HttpResponse(year)
+    return redirect('/2020.html')
+
+
+def name(request, **kwargs):
+    return HttpResponse(kwargs['name'])
+
+
+def myyear(request, year):
+    return render(request, 'yearview.html')
